9083dd791dcecc7d64c09c93a10d3b28fb155f8e
AtirhGael <atirhg@gmail.com>
2024-04-29 14:57:59 +0100

    tem

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = ['Welcome', 'pictures', 'about'];
+const pages = ['Welcome', 'pictures', 'about','Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function ResponsiveAppBar() {
